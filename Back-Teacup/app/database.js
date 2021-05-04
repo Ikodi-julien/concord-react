066052b9708a39dc2546b1368dbadb27a64ca49d
Caitlin <caitlin.sigrist@gmail.com>
2021-05-04 19:20:00 +0200

    chore: models created

--- Back-Teacup/app/database.js
+++ Back-Teacup/app/database.js
@@ -1,15 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.PG_URL, {
-        define: {
-            timestamps: true,
-            //on accepte les champs en snake_case
-            underscored: true,
-            //on indique à sequelize comment sont nommés nos champs de timestamps
-            createdAt: 'created_at',
-            updatedAt: 'updated_at'
-        }
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 // -----------------------------------
 
---
+++ Back-Teacup/database/migrations/20210504170905-create-user.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nickname: {
+        type: {
+          type: DataTypes.TEXT,
+          allowNull: false
+        }
+      },
+      email: {
+        type: {
+          type: DataTypes.TEXT,
+          unique: true,
+          allowNull: false
+        }
+      },
+      password: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('user');
+  }
+};
