510c4869340a297f146a6b3cb51e4da4db601d84
Caitlin <caitlin.sigrist@gmail.com>
2021-05-05 18:06:01 +0200

    feature: update of channel's endpoint

--- Back-Teacup/app/controllers/channel.controller.js
+++ Back-Teacup/app/controllers/channel.controller.js
@@ -1,10 +1,19 @@
 const { Channel } = require("../models");
 
 const channelController = {
-    getChannelById : async (req, res) => {
-        const channel = await Channel.findByPk(req.params.id);
+    getChannelById: async (req, res) => {
 
-        if(!channel){
+        const channel = await Channel.findByPk(req.params.id, {
+            include: {
+                association: 'users',
+                attributes: ['id', 'nickname'],
+                through : {
+                    attributes : []
+                }
+            }
+        });
+
+        if (!channel) {
             return res.status(404).send('Channel not found')
         };
 
