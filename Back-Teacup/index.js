e036bf142aed62f14cc73b5dff36d2259c36aaf3
Caitlin <caitlin.sigrist@gmail.com>
2021-05-05 14:16:43 +0200

    feature: socket.io setup

--- Back-Teacup/index.js
+++ Back-Teacup/index.js
@@ -1,17 +1,34 @@
-const express = require('express');
-const app = express();
 require('dotenv').config();
 
-const apiRouter = require('./app/router');
-const { User } = require('./app/models');
+const express = require('express');
+const { Server } = require('socket.io');
+const { createServer } = require('http')
 
-// test connection db
-const db = require('./app/database');
-//
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+const apiRouter = require('./app/router');
+
+// // test connection db
+// const db = require('./app/database');
+// //
 
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+
 app.use('/v1', apiRouter);
 
-app.listen(PORT, () => console.log(`Serveur running on http://localhost:${PORT}`));
+io.on('connection', socket => {
+    socket.on('auth', ({ channelId, userId }) => {
+        socket.join(`channel-${channelId}`);
+        io.to(`channel-${channelId}`).emit('message', `User ${userId} entered`);
+    })
+
+    socket.on('message', ({ channelId, message }) => {
+        io.to(`channel-${channelId}`).emit('message', message);
+    })
+})
+
+httpServer.listen(PORT, () => console.log(`Serveur running on http://localhost:${PORT}`));
