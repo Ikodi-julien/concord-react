ecfc6f707ab2cbd79f760dc569bc09f703ea8662
Caitlin <caitlin.sigrist@gmail.com>
2021-05-05 17:17:03 +0200

    feature: addition of a way to keep user list accurate

--- Back-Teacup/index.js
+++ Back-Teacup/index.js
@@ -10,52 +10,52 @@ const io = new Server(httpServer);
 
 const apiRouter = require('./app/router');
 
-// // test connection db
-// const db = require('./app/database');
-// //
-
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
 app.use('/v1', apiRouter);
 
-
-/*
-auth => 
-à recevoir
-{
-    user : {
-        id : number,
-        nickname : string
-    }
-    channel : {
-        id : number
-    }
-}
-
-message => 
-à recevoir et renvoyer
-{
-    user : {
-        id : number,
-        nickname : string
-    }
-    channel : {
-        id : number
-    }
-    content : string
-}
-*/
-
 io.on('connection', socket => {
-    socket.on('auth', ({ channel }) => {
+    socket.on('auth', ({ channel/*, user*/ }) => {
+        /*
+        {
+            user : {
+                id : number,
+                nickname : string
+            }
+            channel : {
+                id : number
+            }
+        }
+        */
+
         socket.join(`channel-${channel.id}`);
+
+        // key 'user-join' to tell front to add user to user list
+        // io.to(`channel-${message.channel.id}`).emit('user-join', {channel, user})
     })
 
     socket.on('message', message => {
+        /*
+        {
+            user : {
+                id : number,
+                nickname : string
+            }
+            channel : {
+                id : number
+            },
+            content : string
+        }
+        */
         io.to(`channel-${message.channel.id}`).emit('message', message);
     })
+
+    // socket.on('disconnect', ({ channel, user }) => {
+    //     // key 'user-join' to tell front to add user to user list
+    //     io.to(`channel-${channel.id}`).emit('user-leave', {channel, user});
+    // })
 })
 
 httpServer.listen(PORT, () => console.log(`Serveur running on http://localhost:${PORT}`));
