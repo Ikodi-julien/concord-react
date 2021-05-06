5cef5bf51d1479f6e136f190c68e968f67a8339b
Caitlin <caitlin.sigrist@gmail.com>
2021-05-06 14:25:18 +0200

    feature: addition of tests on api.http & removal of placeholder's route

--- Back-Teacup/app/router.js
+++ Back-Teacup/app/router.js
@@ -1,22 +1,13 @@
 const { Router } = require('express');
 const router = Router();
 
-
 const {authController, channelController, userController} = require('./controllers');
-
-
-
-router.get('/', (req, res) => {
-    res.send('I am a placeholder !')
-})
-
-
-// route pour obtenir les informations d'un utilisateur (uniquement de la table user)
-router.get('/user/:id', userController.getUser);
 
 // route post pour l'enregistrement
 router.post('/signup', authController.signup);
 
+// route pour obtenir les informations d'un utilisateur (uniquement de la table user)
+router.get('/user/:id', userController.getUser);
 
 router.get('/channel/:id', channelController.getChannelById);
 
