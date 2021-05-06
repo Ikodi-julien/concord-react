aba19f52e5c0879b30cb5f12e5d8752f21c624ef
Caitlin <caitlin.sigrist@gmail.com>
2021-05-06 14:28:12 +0200

    feature: removal of user/id endpoint

--- Back-Teacup/app/controllers/index.js
+++ Back-Teacup/app/controllers/index.js
@@ -1,9 +1,7 @@
 const authController = require('./auth.controller');
 const channelController = require('./channel.controller');
-const userController = require('./user.controller');
 
 module.exports = {
     authController,
-    channelController,
-    userController
+    channelController
 }
--- Back-Teacup/app/router.js
+++ Back-Teacup/app/router.js
@@ -1,13 +1,10 @@
 const { Router } = require('express');
 const router = Router();
 
-const {authController, channelController, userController} = require('./controllers');
+const {authController, channelController } = require('./controllers');
 
 // route post pour l'enregistrement
 router.post('/signup', authController.signup);
-
-// route pour obtenir les informations d'un utilisateur (uniquement de la table user)
-router.get('/user/:id', userController.getUser);
 
 router.get('/channel/:id', channelController.getChannelById);
 
