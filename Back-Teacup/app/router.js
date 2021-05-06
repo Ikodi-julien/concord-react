98e83e3332bc5ba593a0bda9740a49e062ec5772
Caitlin <caitlin.sigrist@gmail.com>
2021-05-06 15:02:11 +0200

    chore: addition of try/catch on getChannelId method and removal of useless placeholder's route

--- Back-Teacup/app/router.js
+++ Back-Teacup/app/router.js
@@ -2,10 +2,6 @@ const { Router } = require('express');
 const channelController = require('./controllers/channel.controller');
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.send('I am a placeholder !')
-})
-
 router.get('/channel/:id', channelController.getChannelById);
 
 module.exports = router;
