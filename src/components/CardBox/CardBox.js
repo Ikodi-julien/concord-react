6f477c6cf8c777c13693ca04f6073f52a9903319
Caitlin <caitlin.sigrist@gmail.com>
2021-05-23 16:53:02 +0200

    chore: css on home's channels' list

--- src/components/CardBox/CardBox.js
+++ src/components/CardBox/CardBox.js
@@ -1,49 +1,51 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {Card, Icon} from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Card, Icon } from 'semantic-ui-react';
 
 import './cardbox.scss';
 
-const CardBox = ({list, deleteChannel, isDeletable, deleteFromMyChannels}) => {
-  
+const CardBox = ({ list, deleteChannel, isDeletable, deleteFromMyChannels }) => {
+
   const array = list || [];
   const handleDelete = (evt) => {
     const channelId = evt.target.closest('.cardbox__button').id;
     deleteFromMyChannels(channelId);
   }
   return (
-  <div className="discovery__listcontainer">
-    {
-      array.map(item => (
-        <Card key={item.id} >
-          {isDeletable && <button 
-            id={item.id}
-            className='cardbox__button'
-            onClick={handleDelete}
+    <div className="discovery__listcontainer">
+      <Card.Group>
+      {
+        array.map(item => (
+          <Card key={item.id} >
+            {isDeletable && <button
+              id={item.id}
+              className='cardbox__button'
+              onClick={handleDelete}
             >
-            <i className="fas fa-times"></i>
-          </button>}
-          <Card.Content>
-            <Card.Header>
-              <Link to={`/channels/${item.id}`} >
-              {item.title}
-              </Link>
-              <div className='cardbox__emptyspace' />
-            </Card.Header>
-            <Card.Description>
-              {item.tags &&
-                item.tags.map(tag => <div key={`tag-${tag.id}`} className={"discovery__listcontainer__cardtag" + (tag.matchingTag ? "--matching_tag" : "")}>{tag.name}</div>)
-              }
-            </Card.Description>
-          </Card.Content>
-          <Card.Content extra>
+              <i className="fas fa-times"></i>
+            </button>}
+            <Card.Content>
+              <Card.Header>
+                <Link to={`/channels/${item.id}`} >
+                  {item.title}
+                </Link>
+                <div className='cardbox__emptyspace' />
+              </Card.Header>
+              <Card.Description>
+                {item.tags &&
+                  item.tags.map(tag => <div key={`tag-${tag.id}`} className={"discovery__listcontainer__cardtag" + (tag.matchingTag ? "--matching_tag" : "")}>{tag.name}</div>)
+                }
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
               <Icon name='user' />
               {item.usersCount}
-          </Card.Content>
-        </Card>
-      ))
-    }
-  </div>
+            </Card.Content>
+          </Card>
+        ))
+      }
+    </Card.Group>
+  </div >
 )}
 
 export default CardBox;
