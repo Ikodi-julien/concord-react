0abc8f131db80edd6e7d66e3256c3a2b8fcfc4a7
ikodi-julien <jupellin39@gmail.com>
2021-05-30 11:52:06 +0200

    separated server for react-build chatroom

--- src/vars.js
+++ src/vars.js
@@ -1,9 +1,6 @@
-/* AWS */
-// export const FETCH_URL = 'http://54.159.87.238:8000';
-// export const WS_URL = 'http://54.159.87.238:8000';
 /* HEROKU */
-// export const FETCH_URL = 'https://teacup-back.herokuapp.com';
-// export const WS_URL = 'https://teacup-back.herokuapp.com';
+export const FETCH_URL = 'https://chat-api-ikodi.herokuapp.com';
+export const WS_URL = 'https://chat-api-ikodi.herokuapp.com';
 /* localhost */
-export const FETCH_URL = 'http://localhost:8000';
-export const WS_URL = 'http://localhost:8000';
+// export const FETCH_URL = 'http://localhost:8000';
+// export const WS_URL = 'http://localhost:8000';
