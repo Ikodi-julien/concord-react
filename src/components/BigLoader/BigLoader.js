7cee9d9b6cd9fe0a447f97f7f6c19f05f3b5af52
ikodi-julien <jupellin39@gmail.com>
2021-06-06 13:34:18 +0200

    Fix bug loader

--- src/components/BigLoader/BigLoader.js
+++ src/components/BigLoader/BigLoader.js
@@ -4,11 +4,9 @@ import './bigloader.scss';
 
 const BigLoader = () => (
 <div className="bigloader" >
-  <Segment>
     <Dimmer active>
       <Loader active size='massive'>Loading</Loader>
     </Dimmer>
-  </Segment>
 </div>
 )
 
