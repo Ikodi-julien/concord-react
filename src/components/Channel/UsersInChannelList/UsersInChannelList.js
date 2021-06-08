d749f4ffe60637534a502c18537372d387717747
ikodi-julien <jupellin39@gmail.com>
2021-06-08 18:32:34 +0200

    Some scss, remove borders

--- src/components/Channel/UsersInChannelList/UsersInChannelList.js
+++ src/components/Channel/UsersInChannelList/UsersInChannelList.js
@@ -2,12 +2,9 @@ import React from 'react';
 import defaultAvatar from 'src/assets/avatarDefault.js';
 
 const UsersInChannelList = ({users, showUsersInChannel, toggleUsersInChannel}) => {
-  
   // ici il y a la liste des users connectés au chat et ceux qui l'ont été.
-  
   return (
     <section className={showUsersInChannel ? "usersinchannellist --show" : "usersinchannellist"}>
-      <div className="usersinchannellist__deco" />
     
       <div className="usersinchannellist__header">
         <h1 className="usersinchannellist__title">Utilisateurs</h1>
