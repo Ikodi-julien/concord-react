edca88ab3e0d67d27863b46b986a11c0328b1289
ikodi-julien <jupellin39@gmail.com>
2021-12-30 15:16:58 +0100

    Fix error msg on first login atempt 2, atempt 1

--- src/selectors/handleAPIError.js
+++ src/selectors/handleAPIError.js
@@ -1,24 +1,21 @@
-import {
-  appInfo,
-  hideInfos,
-  FETCH_NAV_DATA,
-} from 'src/actions/appActions';
+/* eslint-disable quotes */
+import { appInfo, hideInfos, FETCH_NAV_DATA } from "src/actions/appActions";
 import {
   FETCH_MY_PROFILE,
   UPDATE_PROFILE,
   SUBMIT_DELETE_AUTH_ACCOUNT,
-} from 'src/actions/profileActions';
+} from "src/actions/profileActions";
 import {
   FETCH_CHANNEL,
   UPDATE_CHANNEL_USERS,
-} from 'src/actions/channelActions';
+} from "src/actions/channelActions";
 import {
   FETCH_MY_CHANNELS,
   FETCH_MY_RECOS,
   DELETE_FROM_MY_CHANNELS,
   UPDATE_AVATAR,
   GET_USER_INFOS,
-} from 'src/actions/userActions';
+} from "src/actions/userActions";
 import {
   SUBMIT_LOGIN_FORM,
   SUBMIT_SIGNUP_FORM,
@@ -31,13 +28,13 @@ import {
   disconnectUserError,
   forgotPassInfo,
   updatePassInfo,
-} from 'src/actions/authActions';
+} from "src/actions/authActions";
 
 export default (error, store, actionType) => {
   const errorTimer = 2500;
   let errorMsg;
 
-  console.warn('Error handled: ', actionType, error.toString());
+  console.warn("Error handled: ", actionType, error.toString());
 
   switch (actionType) {
     case FETCH_CHANNEL:
@@ -49,8 +46,13 @@ export default (error, store, actionType) => {
     case UPDATE_CHANNEL_USERS:
     case DELETE_FROM_MY_CHANNELS:
     case UPDATE_AVATAR:
-      if (error.toString().includes('401')) { // Token expired
-        store.dispatch(appInfo('Les informations d\'identification ne sont plus valables, vous allez être redirigé vers la page d\'accueil pour vous reconnecter'));
+      if (error.toString().includes("401")) {
+        // Token expired
+        store.dispatch(
+          appInfo(
+            "Les informations d'identification ne sont plus valables, vous allez être redirigé vers la page d'accueil pour vous reconnecter"
+          )
+        );
 
         setTimeout(() => {
           store.dispatch(hideInfos());
@@ -58,8 +60,15 @@ export default (error, store, actionType) => {
         }, errorTimer);
         break;
       }
+      if (error.toString().includes("Cannot read properties of undefined ")) {
+        // this is a first login after new create account, i need to do better, i know
+        // TODO
+        break;
+      }
 
-      store.dispatch(appInfo(`Le serveur a renvoyé une erreur: ${error.toString()}`));
+      store.dispatch(
+        appInfo(`Le serveur a renvoyé une erreur: ${error.toString()}`)
+      );
 
       setTimeout(() => {
         store.dispatch(hideInfos());
@@ -68,7 +77,7 @@ export default (error, store, actionType) => {
       break;
 
     case DISCONNECT_USER:
-      store.dispatch(appInfo('Vous êtes déconnecté(e)'));
+      store.dispatch(appInfo("Vous êtes déconnecté(e)"));
       setTimeout(() => {
         store.dispatch(hideInfos());
       }, errorTimer);
@@ -86,16 +95,15 @@ export default (error, store, actionType) => {
       break;
 
     case SUBMIT_UPDATE_PASSWORD:
-      if (error.toString().includes('409')) {
-        errorMsg = 'Mot de passe incorrect';
+      if (error.toString().includes("409")) {
+        errorMsg = "Mot de passe incorrect";
       }
-      if (error.toString().includes('409')) {
-        errorMsg = 'Désolé, il faut vous reconnecter';
+      if (error.toString().includes("409")) {
+        errorMsg = "Désolé, il faut vous reconnecter";
       }
       if (errorMsg) {
         store.dispatch(appInfo(errorMsg));
-      }
-      else {
+      } else {
         store.dispatch(appInfo(error.toString()));
       }
       setTimeout(() => {
@@ -111,7 +119,7 @@ export default (error, store, actionType) => {
       break;
 
     default:
-      if (typeof error.response !== 'undefined') {
+      if (typeof error.response !== "undefined") {
         store.dispatch(appInfo(error.response.data.message));
         setTimeout(() => {
           store.dispatch(hideInfos());
